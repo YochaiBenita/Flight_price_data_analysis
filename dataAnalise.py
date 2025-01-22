46028f6effbaef988300bc73f844a73129415df1
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-22 10:56:31 +0200

    files

---
+++ dataAnalise.py
@@ -0,0 +1,28 @@
+import csv
+import os
+
+import duckdb
+from pandas.io.common import file_exists
+
+# creating small example
+with open("itineraries.csv", 'r', newline='') as infile, \
+        open("itineraries_500.csv", 'w', newline='') as outfile:
+
+    reader = csv.reader(infile)
+    writer = csv.writer(outfile)
+
+    # Write header row
+    header = next(reader)
+    writer.writerow(header)
+
+    # Write specified number of data rows
+    for i, row in enumerate(reader):
+        if i >= 500:
+            break
+        writer.writerow(row)
+
+if not file_exists("database.duckdb"):
+    duckdb_conn = duckdb.connect("database.duckdb")
+    duckdb_conn.execute("CREATE TABLE main AS SELECT * FROM read_csv_auto('itineraries.csv')")
+else:
+    duckdb_conn = duckdb.connect("database.duckdb")
