6ec1d62471012c989997a68250ffc7ed2107aceb
Benjamin Rosin <benjaminro@std.hac.ac.il>
2025-01-22 11:54:19 +0200

    word

--- dataAnalise.py
+++ dataAnalise.py
@@ -1,14 +1,16 @@
 import csv
 import os
 
+
 import duckdb
 from pandas.io.common import file_exists
 
-# creating small example
+# creating small csv example
 if not file_exists("itineraries_500.csv"):
-    with open("itineraries.csv", 'r', newline='') as infile, \
-            open("itineraries_500.csv", 'w', newline='') as outfile:
-
+    with (
+        open("itineraries.csv", "r", newline="") as infile,
+        open("itineraries_500.csv", "w", newline="") as outfile,
+    ):
         reader = csv.reader(infile)
         writer = csv.writer(outfile)
 
@@ -22,14 +24,38 @@ if not file_exists("itineraries_500.csv"):
                 break
             writer.writerow(row)
 
+# connecting to duckDB, creating table if needed
 if not file_exists("database.duckdb"):
     duckdb_conn = duckdb.connect("database.duckdb")
-    duckdb_conn.execute("CREATE TABLE main AS SELECT * FROM read_csv_auto('itineraries.csv')")
+    duckdb_conn.execute(
+        "CREATE TABLE main AS SELECT * FROM read_csv_auto('itineraries.csv')"
+    )
     print("duckDB file created")
 else:
     duckdb_conn = duckdb.connect("database.duckdb")
     print("duckDB file connected")
 
+# remove old tables
+duckdb_conn.execute("DROP TABLE IF EXISTS sample")
+duckdb_conn.execute("DROP TABLE IF EXISTS query1")
+duckdb_conn.execute("DROP TABLE IF EXISTS query2")
+duckdb_conn.execute("DROP TABLE IF EXISTS query3")
+duckdb_conn.execute("DROP TABLE IF EXISTS query4")
+
+# creating sample data
+duckdb_conn.execute("CREATE TABLE sample AS SELECT * FROM main USING SAMPLE 500;")
+
+# query 1
+duckdb_conn.execute()
+
+# query 2
+duckdb_conn.execute()
+
+# query 3
+duckdb_conn.execute()
+
+# query 4
+duckdb_conn.execute()
 
 
 
